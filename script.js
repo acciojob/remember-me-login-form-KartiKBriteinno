307bd051256ed6e023b356626dcf9e783da2c5c3
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-06-10 13:08:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
     const usernameInput = document.getElementById('username');
